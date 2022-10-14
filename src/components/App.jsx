d5b47b4e5cd021b040928ac1a5a4f59af916931c
Dafarlog <Dafarlog@gmail.com>
2022-10-14 22:39:36 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
+import {Feedback} from "components/Feedback/Feedback"
 import { Phonebook } from "./Phonebook/Phonebook";
+
 export const App = () => {
   return (
-    <Phonebook/>
+    <>
+      <Phonebook/>
+    </>
+    
   );
 };
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,42 +1,34 @@
 import React from "react";
 import nextId from "react-id-generator";
 
-import { Contacts } from "components/Contacts/Contacts";
-import { FormAddContacts } from "components/FormAddContacts/FormAddContacts";
+import { ContactsList } from "components/ContactsList/ContactsList";
+import { ContactForm } from "components/ContactForm/ContactForm";
+import {Filter} from "components/Filter/Filter";
+import { Section } from "components/Section/Section";
+
 
 export class Phonebook  extends React.Component{
 
     state = {
         contacts: [],
-        filter: '',
-        options: {
-          name: "", 
-          number: '', 
-        }    
-    }
-
-    changeHandler = (e) =>{
-        const {name, value} = e.target;
-        this.setState( state =>({ options:{ ...state.options, [name]: value}}));
-        
+        filter: '', 
     }
     searcheHandler = (e) =>{
-        this.setState(state=>({filter: e.target.value}))
+        this.setState(state=>({filter: e.target.value.toLowerCase()}))
     }
-    onSubmit = (e) => {
-        e.preventDefault();
-        if(this.state.contacts.findIndex(elem => elem.name === this.state.options.name) !== -1){
+    onSubmit = (data) => {
+        console.log(data);
+        if(this.state.contacts.findIndex(elem => elem.name === data.name) !== -1){
             alert("Contact already exists")
             return
         }
         this.setState( state => {
             return(
             {
-                contacts: [...state.contacts, {id: nextId(), name: state.options.name, number: state.options.number}],
+                contacts: [...state.contacts, {id: nextId(), name: data.name, number: data.number}],
             })
         })
     }
-    
 
     deleteContact = (e) =>{
         const index = this.state.contacts.findIndex(elem => elem.id === e.target.dataset.id)
@@ -44,6 +36,7 @@ export class Phonebook  extends React.Component{
         this.setState( state => {
             const contacts = state.contacts;
             contacts.splice(index, 1);
+            console.log(contacts);
             return(
             {
                 contacts: [...contacts],
@@ -51,32 +44,17 @@ export class Phonebook  extends React.Component{
         })
     }
 
-    getFromStoreg = () =>{
-        return JSON.parse(localStorage.getItem("contacts"));
-    }
-
-    setToStorage =()=>{
-        localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-
-    componentDidMount(){
-        this.setState({contacts: this.getFromStoreg()});
-    }
-
-    componentDidUpdate(){
-        this.setToStorage();
-    }
-
-
     render(){
         return(
-            
             <>
-            <FormAddContacts  inputFunc={this.changeHandler} submitFunc = {this.onSubmit}/>
-            <Contacts contacts={this.state.contacts} 
-            filter={this.state.filter} 
-            searcheFunc = {this.searcheHandler}
-            deletFunc = {this.deleteContact}/>
+            <ContactForm  inputFunc={this.changeHandler} submitFunc = {this.onSubmit}/>
+            <Section title="Contacts">
+                <Filter searcheFunc = {this.searcheHandler}/>
+                <ContactsList contacts={this.state.contacts} 
+                filter={this.state.filter} 
+                deletFunc = {this.deleteContact}/>
+            </Section>
+            
             </>
             
         )
